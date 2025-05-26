b636095c6a6bd62e0fa6789b491910236ad90690
Felipe Amorim <felipe21fab@gmail.com>
2025-05-26 16:04:12 -0300

    [FEAT] Created fetch for pokemon api

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async ()=> {
+      try{
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+        if(!response){
+          throw new Error("Erro na requisição");
+        }
+        const result = await response.json();
+        setData(result);
+      }
+      catch(err){
+        setError(err);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
